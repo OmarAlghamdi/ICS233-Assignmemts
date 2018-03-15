9cb8f6d4ac02fc061e68bbf991c359ec36821af1
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2018-03-15 22:24:21 +0300

    write merge method

--- Assignment 2/MergeSort.asm
+++ Assignment 2/MergeSort.asm
@@ -46,6 +46,53 @@ print_array:	la $a0, msg1
 		addi $t0, $t0, 4
 		blt $t0, $s2, loop2
 		jr $ra
-merge:
+merge:		sll $t0, $a1, 2		# allocate B[] on the stack
+		move $fp, $sp		# B[n-1]
+		add $sp,$sp, $t0	# B[0]
+		srl $t0, $a1, 1		# t0 = m = n/2
+		move $t1, $zero		# t1 = i = 0
+		move $t2, $t0		# t2 = j = m 
+		move $t3, $zero		# t3 = k = 0
+	while1:	bge $t1, $t0, while2	# i !< m
+		bge $t2, $a1, while2	# j !< n
+		sll $t4, $t1, 2		# A[i]
+		add $t4, $a0, $t4	
+		lw $t4, ($t4)
+		sll $t5, $t2, 2		# A[j}
+		add $t5, $a0, $t5 	
+		lw $t5, ($t5)
+		sll $t6, $t3, 2		# B[k]
+		add $t6, $t6, $sp
+	if:	bgt $t4, $t5, else
+		sw $t4, ($t6)		# B[k] = A[i]
+		addi $t1, $t1, 1	# i++
+		addi $t3, $t3, 1	# k++
+		j while1
+	else:	sw $t5, ($t6)		# B[k] = A[j]
+		addi $t2, $t2, 1	# j++
+		addi $t3, $t3, 1	# k++
+		j while1
+	while2:	bge $t1, $t0, for	# i !< m
+		sll $t4, $t1, 2		# A[i]
+		add $t4, $a0, $t4	
+		lw $t4, ($t4)
+		sll $t6, $t3, 2		# B[k]
+		add $t6, $t6, $sp
+		sw $t4, ($t6)
+		addi $t1, $t1, 1	# i++
+		addi $t3, $t3, 1	# k++
+		j while2
+	for:	move $t1, $zero		# i = 0
+	loop3:	sll $t4, $t1, 2		# A[i]
+		add $t4, $a0, $t4	
+		sll $t6, $t1, 2		# B[i]
+		add $t6, $sp, $t6	
+		lw $t6, ($t6)
+		sw $t6, ($t4)		# A[i] = B[i]
+		addi $t1, $t1, 1	# i++
+		blt $t1, $t3, loop3	# i < k
+		move $sp, $fp		# deallocate 
+		jr $ra
+		
 merge_sort:
 
