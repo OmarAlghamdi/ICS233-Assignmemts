3b0154bfd88a5c45fbb0f07fa63c7d6ae7c01f6b
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2018-03-20 00:57:47 +0300

    a) & b) tested

--- Assignment 3/Floating Matrix.asm
+++ Assignment 3/Floating Matrix.asm
@@ -1,24 +1,25 @@
 # Omar Alghamdi
 # 201528950
 .data
-fMatrix:	.float	0:100
-prompt1:	.asciiz	"Choose what you want to do\m"
-prompt2:	.asciiz	"Enter the size of the matrix. size should be from 2 to 10"
-prompt3:	.asciiz	"Enter matrix elements row by row"
+fMatrix:	.float	0:400
+prompt1:	.asciiz	"Choose what you want to do\n"
+prompt2:	.asciiz	"Enter the size of the matrix. size should be from 2 to 10\t"
+prompt3:	.asciiz	"Enter matrix elements row by row\n"
 choice1:	.asciiz	"1. Enter a Matrix of Single-Precision Floats\n"
 choice2:	.asciiz	"2. Compute a Row Sum\n"
 choice3:	.asciiz	"3. Compute a Column Sum\n"
 choice4:	.asciiz	"4. Locate the Minimum Element in the Matrix\n"
 choice5:	.asciiz	"5. Exit the Program\n"
 errMSG1:	.asciiz	"invalid choice. please try again"
-errMSG1:	.asciiz	"invalid matrix size. please try again"
+errMSG2:	.asciiz	"invalid matrix size. please try again"
 .text
-main:	jal choice
-	beq $v0, 5, exit
-	la $a0, fMatrix
-	jal size
-exit:	li $v0, 10
-	syscall	
+main:		jal choice
+		beq $v0, 1, sizeFill
+		beq $v0, 5, exit
+sizeFill:	la $a0, fMatrix
+		jal size
+exit:		li $v0, 10
+		syscall	
 
 choice:	la $a0, prompt1	# prints the menu
 	li $v0, 4
@@ -49,30 +50,31 @@ err1:	la $a0, errMSG1	# prints error message
 	j read1
 	
 
-size:	la $a0, prompt2
+size:	move $t0, $a0	# matrix address
+	la $a0, prompt2
 	li $v0, 4
 	syscall
 read2:	li $v0, 5
 	syscall
 	bge $v0, 2, input
 	ble $v0, 10, input
-	la $a0, errMSG1	# prints error message
+	la $a0, errMSG2	# prints error message
 	li $v0, 4
 	syscall
 	j read2
-input:	move $t0, $v0	# matrix size
-	move $t1, $v0	
-	sll $t1, $t1, 4
-	addu $t1, $t1, $a0	# last address
+input:	move $t1, $v0	# matrix size	
+	mul $t2, $v0, $v0
+	sll $t2, $t2, 2
+	addu $t2, $t2, $t0	# last address
 	la $a0, prompt3
 	li $v0, 4
 	syscall
 fill:	li $v0, 5	# reads element
 	syscall
-	sw $v0, ($a0)	# stores element
-	addiu $a0, $a0, 4
-	blt $a0, $t1, fill
-	move $v0, $t0	# returns the size (N)
+	sw $v0, ($t0)	# stores element
+	addiu $t0, $t0, 4
+	blt $t0, $t2, fill
+	move $v0, $t1	# returns the size (N)
 	jr $ra
 
 
